399778a66685c1ae77ab73a2995cf07deff08700
Sammy Torres <storresii1316@gmail.com>
2023-10-24 11:57:00 -0400

    completed profile component

--- react/unit_3/2_creating_new_react_app/src/components/Header.js
+++ react/unit_3/2_creating_new_react_app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <>
-            <h1 className="header">About Me</h1>
+            <h1 className="h-1">About Me</h1>
         </>
     )
 }
--- react/unit_3/2_creating_new_react_app/src/components/Profile.js
+++ react/unit_3/2_creating_new_react_app/src/components/Profile.js
@@ -3,6 +3,15 @@ import React from "react";
 const Profile = () => {
     return (
         <>
+            <h2 className="h-2">Profile</h2>
+            <div className="paragraph">
+                <p>&emsp;My name is Sam. My background comes from customer service and management.  A few years back I decided to take a chance on myself and fulfill a life-long dream to transition into programming.  So I went back to school, FIU, and received a bachelor's in Computer Science.</p>
+
+                <p>&emsp;What intrigues me the most about programming, is the problem-solving aspect.  At times it may be difficult to solve a problem, but I'm a true believer that there is a solution to every problem.  There is no better feeling, than finding a solution to a problem.  This is what really attracted me to programming.</p>
+
+                <p>&emsp;My problem-solving skills, along with my background in management and customer service would make me a valuable asset in your organization.  I understand the importance of working on a team and providing the best product to our customers, but, when necessary, I am able to work on my own and be accountable.</p>
+            </div>
+            
         </>
     )
 }
