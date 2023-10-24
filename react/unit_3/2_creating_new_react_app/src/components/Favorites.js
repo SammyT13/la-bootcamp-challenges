270e724050e59e6ed4c446cf1a39003213ce9382
Sammy Torres <storresii1316@gmail.com>
2023-10-24 13:59:53 -0400

    completed react challenges

--- react/unit_3/2_creating_new_react_app/src/components/Favorites.js
+++ react/unit_3/2_creating_new_react_app/src/components/Favorites.js
@@ -3,6 +3,15 @@ import React from "react";
 const Favorites = () => {
     return (
         <>
+            <h2 className="h-2">Favorite Things To Do</h2>
+            <div className="list">
+                <ul>
+                    <li>Family Time</li>
+                    <li>Programming</li>
+                    <li>Learning</li>
+                    <li>Working-Out</li>
+                </ul>
+            </div>
         </>
     )
 }
