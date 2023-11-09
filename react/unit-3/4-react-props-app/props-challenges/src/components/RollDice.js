cc5502b887ca173e8823a4f929327a81fa3331fe
Sammy Torres <storresii1316@gmail.com>
2023-11-08 19:22:13 -0500

    partially complete

---
+++ react/unit-3/4-react-props-app/props-challenges/src/components/RollDice.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RollDice = (props) => {
+    return(
+        <>
+            <button onClick={() => {
+                props.handlePlayer1Dice(); props.handlePlayer2Dice()
+            }}>Roll The Dice</button>
+        </>
+    )
+}
+
+export default RollDice
